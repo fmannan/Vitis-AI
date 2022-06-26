64569dcc591f147981ebc6398efaa2254241e7da
Fahim Mannan <fmannan@gmail.com>
2022-06-26 00:36:36 -0400

    updated replace pytorch for pytorch 1.7.1

--- docker/dockerfiles/replace_pytorch.sh
+++ docker/dockerfiles/replace_pytorch.sh
@@ -44,7 +44,8 @@ eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
 echo -e "\n#### Creating a new conda environment by cloning vitis-ai-pytorch and activate it..."
 sudo chmod 777 /opt/vitis_ai/conda 
 cd /scratch/
-wget -O conda-channel.tar.gz --progress=dot:mega https://www.xilinx.com/bin/public/openDownload?filename=conda-channel_2.0.0.1103-02.tar.gz
+#wget -O conda-channel.tar.gz --progress=dot:mega https://www.xilinx.com/bin/public/openDownload?filename=conda-channel_2.0.0.1103-02.tar.gz
+wget -O conda-channel.tar.gz --progress=dot:mega https://www.xilinx.com/bin/public/openDownload?filename=conda-channel_2.5.0.1260-01.tar.gz
 tar -xzvf conda-channel.tar.gz
 source /opt/vitis_ai/conda/etc/profile.d/conda.sh
 conda create -n $1  --clone vitis-ai-pytorch  
@@ -69,9 +70,9 @@ mamba uninstall -y pytorch torchvision pytorch_nndct
 echo -e "\n#### Installing pytorch 1.9 packages ..."
 echo -e "\e[91m>>>> Edit this line of command to set the target version, refer to https://pytorch.org/get-started/previous-versions/ <<<<\e[m"
 #### Installing pytorch 1.7.1 packages ...torchvision==0.5.0+cu100 -f https://download.pytorch.org/whl/torch_stable.html
-mamba install -y pytorch==1.9.0 torchvision==0.10.0 torchaudio==0.9.0 cudatoolkit=10.2 -c pytorch
+#mamba install -y pytorch==1.9.0 torchvision==0.10.0 torchaudio==0.9.0 cudatoolkit=10.2 -c pytorch
 #conda install pytorch==1.8.1 torchvision==0.9.1 torchaudio==0.8.1 cudatoolkit=10.2 -c pytorch
-#conda install pytorch==1.7.1 torchvision==0.8.2 torchaudio==0.7.2 cudatoolkit=10.2 -c pytorch
+mamba install pytorch==1.7.1 torchvision==0.8.2 torchaudio==0.7.2 cudatoolkit=10.2 -c pytorch
 #conda install pytorch==1.6.0 torchvision==0.7.0 cudatoolkit=10.2 -c pytorch
 
 if [ $? -eq 0 ]; then
@@ -85,10 +86,10 @@ echo -e "\n#### Checkout code of vai_q_pytorch ..."
 echo -e "\e[91m>>>> You can apply your local code of vai_q_pytorch and comment out the following lines of git command <<<<\e[m"
 git init code_vaiq && cd code_vaiq 
 git config core.sparsecheckout true
-echo 'tools/Vitis-AI-Quantizer/vai_q_pytorch/' >> .git/info/sparse-checkout 
+echo 'src/Vitis-AI-Quantizer/vai_q_pytorch/' >> .git/info/sparse-checkout
 git remote add origin https://github.com/Xilinx/Vitis-AI.git
 git pull origin master
-cd tools/Vitis-AI-Quantizer/vai_q_pytorch/
+cd src/Vitis-AI-Quantizer/vai_q_pytorch/
 
 if [ $? -eq 0 ]; then
   echo -e "\n#### Vai_q_pytorch code is checked out successfully."
